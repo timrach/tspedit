5762a7e18dae4a0e7758626acfa29b11b7faa45a
Tim Rach <timrach@Tims-MacBook-Pro-2.local>
2015-05-01 22:23:01 +0200

    moved code from tsputil to tspio

--- src/tspio.py
+++ src/tspio.py
@@ -2,7 +2,6 @@ import re
 import ast
 import os
 from tkinter.filedialog import asksaveasfile, askopenfile
-from tsputil import *
 
 
 def parseTSPFile(file):
@@ -31,6 +30,31 @@ def parseTSPFile(file):
 
     f.close
     return (nodes, groups)
+
+def getGroups(nodes):
+    """ return an array holding all occuring colorids of the given nodeset"""
+    groups = []
+    for n in nodes:
+        if not n.color in groups:
+            groups.append(n.color)
+    return groups
+
+
+def constructGroupsString(nodes):
+    """ """
+    groups = getGroups(nodes)
+    if (len(groups) <= 1):
+        return ""
+    else:
+        result = []
+        for g in groups:
+            group = []
+            for n in nodes:
+                if(n.color == g):
+                    #+1 because .tsp nodes are indexed with 1
+                    group.append(n.id + 1)
+            result.append(group)
+        return str(result)
 
 
 def importTSP(callback):
--- src/tsputil.py
+++ src/tsputil.py
@@ -1,29 +1,9 @@
+import subprocess
+import os
+import tspio
+
 colors = ["Black", "Red", "Green", "Blue", "Orange", "Cyan",
           "Magenta", "Yellow", "Gray", "White", "Brown",
           "Pink", "Purple", "Violet"]
 
 
-def getGroups(nodes):
-    """ return an array holding all occuring colorids of the given nodeset"""
-    groups = []
-    for n in nodes:
-        if not n.color in groups:
-            groups.append(n.color)
-    return groups
-
-
-def constructGroupsString(nodes):
-    """ """
-    groups = getGroups(nodes)
-    if (len(groups) <= 1):
-        return ""
-    else:
-        result = []
-        for g in groups:
-            group = []
-            for n in nodes:
-                if(n.color == g):
-                    #+1 because .tsp nodes are indexed with 1
-                    group.append(n.id + 1)
-            result.append(group)
-        return str(result)
