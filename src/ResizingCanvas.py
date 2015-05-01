6942e5d6f49b7fe86e47ccdc8cde83cc3f627963
Tim Rach <timrach@Tims-MacBook-Pro-2.local>
2015-05-01 11:37:06 +0200

    Added correct item scaling

--- src/ResizingCanvas.py
+++ src/ResizingCanvas.py
@@ -9,6 +9,8 @@ class ResizingCanvas(tk.Canvas):
 		self.configure(highlightthickness=0)
 		self.height = self.winfo_reqheight()
 		self.width = self.winfo_reqwidth()
+		self.hscale = 1.0
+		self.wscale = 1.0
 		self.fieldsize = 30
 		self.linewidth = 3
 		self.rows = math.floor(self.height / self.fieldsize)
@@ -31,13 +33,14 @@ class ResizingCanvas(tk.Canvas):
 		# determine the ratio of old width/height to new width/height
 		wscale = float(event.width) / self.width
 		hscale = float(event.height) / self.height
-		print(hscale)
 		#bscale = max(wscale,hscale)
 		self.width = event.width
 		self.height = event.height
 		# resize the canvas
 		self.config(width=self.width, height=self.height)
 		# rescale all the objects tagged with the "all" tag
+		self.hscale *= hscale
+		self.wscale *= wscale
 		self.scale("all",0,0,wscale,hscale)
 		#self.drawGrid()
 
@@ -58,16 +61,17 @@ class ResizingCanvas(tk.Canvas):
 
 	def onMotion(self,event):
 		# get relative field coordinates
-		q = math.floor(event.x / self.fieldsize)
-		r = math.floor(event.y / self.fieldsize)
+		q = math.floor(event.x / (self.fieldsize * self.wscale))
+		r = math.floor(event.y / (self.fieldsize * self.hscale))
 		self.parent.updatePositionLabel(q,r)
 
 	def canvas_clicked(self, event):
 		"""Callback for the click-event on the canvas area
 		Draws a point at the clicked position"""
+		print(str(self.hscale) + " " + str(self.hscale))
 		# get relative field coordinates
-		q = math.floor(event.x / self.fieldsize)
-		r = math.floor(event.y / self.fieldsize)
+		q = math.floor(event.x / (self.fieldsize * self.wscale))
+		r = math.floor(event.y / (self.fieldsize * self.hscale))
 		# only do something if the clicked position is within bounds
 		if(q < self.cols and r < self.rows and q >= 0 and r >= 0):
 			point = self.points[r * self.cols + q]
@@ -81,17 +85,19 @@ class ResizingCanvas(tk.Canvas):
 	def clear(self):
 		self.delete(tk.ALL)
 		self.drawGrid()
+		self.scale("all",0,0,self.wscale,self.hscale)
 
 	def addNode(self,x,y,color):
 		""" draws a point on the specified position on the canvas and adds
 		the data to the nodes and points arrays and the nodes listbox"""
 		index = y * self.cols + x
 		self.points[index] = self.create_oval(
-			x * self.fieldsize + self.linewidth2,
-			y * self.fieldsize + self.linewidth2,
-			(x + 1) * self.fieldsize,
-			(y + 1) * self.fieldsize,
+			(x * self.fieldsize + self.linewidth2)*self.wscale,
+			(y * self.fieldsize + self.linewidth2)*self.hscale,
+			((x + 1) * self.fieldsize)*self.wscale,
+			((y + 1) * self.fieldsize)*self.hscale,
 			fill=color)
 	
 	def deleteNode(self,x,y):
 		self.delete(self.points[y * self.cols + x])
+		self.points[y * self.cols + x] = None
