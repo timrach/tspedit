9a2797c5a366777ecdb1a1730ddce46749bd794a
Tim Rach <timrach@Tims-MacBook-Pro-2.local>
2015-05-09 21:24:34 +0200

    implemented observer pattern

--- src/main.py
+++ src/main.py
@@ -20,12 +20,21 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
 
         """ DATA """
-        self.scale = 100  # factor coordinates are scaled with
-        self.selectedColor = 0  # active colorid
-        self.nodes = []  # internal node representation
-        self.solution = []  # container for the drawn solution
+        self._parent = parent
+        self._data = {}
+        self._observer = {}
 
-        """ GUI """
+        self._setupData()
+        self._setupGui()
+        self._setupMenu()
+
+    """ CLASS METHODS """
+
+    def _setupData(self):
+        self._data.update({'scale': 100,
+                           'nodes': []})
+
+    def _setupGui(self):
         self.configure(padx=10, pady=10)
         self.canvas = CanvasFrame(self, relief=tk.SUNKEN)
         self.sidebar = SidebarFrame(self, relief=tk.SUNKEN)
@@ -33,8 +42,8 @@ class MainApplication(tk.Frame):
         self.sidebar.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-        """ MENU BAR """
-        menubar = tk.Menu(parent)
+    def _setupMenu(self):
+        menubar = tk.Menu(self._parent)
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Import .tsp", command=self.importTSP,
                              accelerator="Ctrl+I")
@@ -47,7 +56,7 @@ class MainApplication(tk.Frame):
                              accelerator="Ctrl+T")
         self.bind_all("<Control-t>", self.exportTIKZ)
         filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=parent.quit)
+        filemenu.add_command(label="Exit", command=self._parent.quit)
 
         tspmenu = tk.Menu(menubar, tearoff=0)
         tspmenu.add_command(label="Solve tsp", command=self.solveTSP,
@@ -57,9 +66,11 @@ class MainApplication(tk.Frame):
                             command=self.showConvexHull,
                             accelerator="Ctrl+H")
         self.bind_all("<Control-h>", self.showConvexHull)
-        tspmenu.add_command(label="Clear path", command=self.clearPath,
+        tspmenu.add_command(label="Clear path",
+                            command=lambda: self.commitChange('solution', []),
                             accelerator="Ctrl+Shift+P")
-        self.bind_all("<Control-Shift-p>", self.clearPath)
+        self.bind_all("<Control-Shift-p>",
+                      lambda: self.commitChange('solution', []))
 
         editormenu = tk.Menu(menubar, tearoff=0)
         editormenu.add_command(label="Clear Data", command=self.clear,
@@ -69,16 +80,57 @@ class MainApplication(tk.Frame):
         menubar.add_cascade(label="File", menu=filemenu)
         menubar.add_cascade(label="TSP", menu=tspmenu)
         menubar.add_cascade(label="Editor", menu=editormenu)
-        parent.config(menu=menubar)
+        self._parent.config(menu=menubar)
 
-    """ CLASS METHODS """
+    def registerData(self, key, value):
+        """Adds a new entry to the data dictionary """
+        self._data.update({key: value})
+
+    def unregisterData(self, key):
+        """Removes a data entry from the data dictionary """
+        del self._data[key]
+
+    def registerObserver(self, observer):
+        self._observer.append(observer)
+
+    def unregisterObserver(self, observer):
+        self._observer.remove(observer)
+
+    def commitChange(self, key, value):
+        self._data[key] = value
+        self.notify(key)
+
+    def notify(self, key='all'):
+        ndata = self._data
+        if key is not 'all':
+            ndata = self._data[key]
+
+        for o in self._observer:
+            o.update(ndata)
 
     def clear(self, event=None):
         """ Clears all problem data from the program and resets the UI """
-        self.nodes = []
-        self.solution = []
-        self.canvas.clear()
-        self.sidebar.clear()
+        self._data = {}
+        for o in self._observer:
+            o.clear()
+
+    def putSolution(self, solution):
+        """ Update the solution data and pass it to the sidebar and canvas.
+        The solution is a list of indices."""
+        solution.append(solution[0])  # append startnode to make tour circular
+        self.commitChange('solution', solution)
+
+    def addNode(self, xc, yc):
+        """ Add a node to the nodes array
+        and pass it to the GUI"""
+        # update data
+        node_list = self._data['nodes']
+        color = self._data['selectedColor']
+
+        new_node = Node(len(node_list), xc, yc, color)
+        new_nodelist = node_list.append(new_node)
+
+        self.commitChange('nodes', new_nodelist)
 
     def solveTSP(self, event=None):
         """ Call the util module to solve the currently drawn
@@ -87,104 +139,73 @@ class MainApplication(tk.Frame):
         on .tsp files, the drawn problem is exported to a temporary file"""
         dummy = tsputil.FilenameWrapper("tmpfile.tsp")
         tspio.exportTSP(
-            self.nodes, self.scale,
+            self._data['nodes'], self._data['scale'],
             lambda f: tsputil.solveTSP(f, self.putSolution), dummy)
 
     def showConvexHull(self, event=None):
         """ Call the util module to calculate the convex hull and
         pass self.putSolution as the callback."""
-        solution = tsputil.get_convex_hull(self.nodes)
+        solution = tsputil.get_convex_hull(self._data['nodes'])
         # map nodes to their ids
         solution = list(map(lambda n: n.id, solution))
         self.putSolution(solution)
 
-    def clearPath(self, event=None):
-        """ Erase all solution data """
-        self.solution = []
-        self.sidebar.removePathInfo()
-        self.canvas.clearPath()
-
-    def putSolution(self, solution):
-        """ Update the solution data and pass it to the sidebar and canvas.
-        The solution is a list of indices."""
-        solution.append(solution[0])  # append startnode to make tour circular
-        self.solution = solution
-        self.canvas.putSolution(self.nodes, solution)
-        self.sidebar.addPathInfo(self.nodes, solution)
-
     def exportTSP(self, event=None):
         """ Export the loaded problem via the IO module in .tsp format"""
         tspio.exportTSP(
-            self.nodes, self.scale, lambda f: self.sidebar.setFilename(f))
+            self._data['nodes'], self._data['scale'],
+            lambda f: self.commitChange('filename', f))
 
     def exportTIKZ(self, event=None):
         """ Export the loaded problem via the IO module as a tikz graphic
         in .tex format"""
-        tspio.exportTIKZ(self.nodes, self.scale)
+        tspio.exportTIKZ(self._data['nodes'], self._data['scale'])
 
     def importTSP(self, event=None):
         """ Load data from a .tsp file via the IO module """
         tspio.importTSP(self.putLoadedData)
 
-    def nodeSelected(self, coords):
-        """ Gets called when the user selects node in the canvas.
-        Passes the node coordinates to the sidebar module"""
-        self.sidebar.nodeSelected(coords)
-
-    def nodeDeselected(self):
-        """ Gets called when the user deselects node in the canvas.
-        Calls the corresponding method in the sidebar module"""
-        self.sidebar.nodeDeselected()
-
     def putLoadedData(self, filename, nodes, groups):
         """ Fills the internal data structures with the loaded data.
             !!! No error handling or validity checks !!!"""
         self.clear()
-        self.sidebar.setFilename(filename)
+
+        node_list = []
         # If the nodes are not grouped, draw them in the currently
         # selected color
         if groups == []:
+            color = self._data['selectedColor']
             for node in nodes:
-                # node[0] holds the x-coordinate, node[1] holds the
-                # y-coordinate
-                self.addNode(
-                    int(node[0] / self.scale), int(node[1] / self.scale))
+                new_node = Node(len(node_list),
+                                int(node[0] / self._data['scale']),
+                                int(node[1] / self._data['scale']), color)
+                node_list.append(new_node)
         # if the nodes are grouped, draw nodes from the same group in the same
         # color
         else:
-            color_old = self.selectedColor  # remember selected color
             # iterate over groups
             for (i, g) in enumerate(groups):
                 # iterate over node ids in the group
                 for e in g:
-                    # set color
-                    self.selectedColor = i
                     # get node coordinates
                     node = nodes[e - 1]
-                    # draw the node
-                    self.addNode(
-                        int(node[0] / self.scale), int(node[1] / self.scale))
-            self.selectedColor = color_old
+                    new_node = Node(len(node_list),
+                                    int(node[0] / self._data['scale']),
+                                    int(node[1] / self._data['scale']), i)
+                    node_list.append(new_node)
 
-    def addNode(self, xc, yc):
-        """ Add a node to the nodes array
-        and pass it to the canvas and sidebar"""
-        # update data
-        new_node = Node(len(self.nodes), xc, yc, self.selectedColor)
-        self.nodes.append(new_node)
-        # pass data to frames
-        self.canvas.addNode(xc, yc, tsputil.colors[self.selectedColor])
-        self.sidebar.addNode(new_node)
+        self.commitChange('filename', filename)
+        self.commitChange('nodes', node_list)
 
     def deleteNode(self, xc, yc):
         """ removes a node form the data structures and the canvas.
         As the nodes are stored as a list, the ids of the nodes after
-        the delted one have to be updated """
-        self.canvas.deleteNode(xc, yc)
+        the deleted one have to be updated """
         # find nodeobject in nodes list
         found = False
         target = None
-        for n in self.nodes:
+        node_list = self._data['nodes']
+        for n in node_list:
             if not found:
                 if n.x == xc and n.y == yc:
                     target = n
@@ -192,11 +213,11 @@ class MainApplication(tk.Frame):
             else:
                 # update id
                 n.id = n.id - 1
-        self.nodes.remove(target)
-        self.sidebar.deleteNode(target.id)
+        node_list.remove(target)
+        self.commitChange('nodes', node_list)
 
-    def getNodes(self):
-        return self.nodes
+    def getData(self, key):
+        return self._data[key]
 
 
 if __name__ == '__main__':
