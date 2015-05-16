a89ef4c3483c4e778da02b5cf6da7ce65c15de2d
Tim Rach <timrach@Tims-MacBook-Pro-2.local>
2015-05-16 17:52:01 +0200

    removed goc and com checkboxes, added info label

--- src/BottomBarWidget.py
+++ src/BottomBarWidget.py
@@ -14,32 +14,18 @@ class BottomBarWidget(tk.Frame):
         self._parent = parent
         self._datacontroller = datacontroller
 
-        self.keywords = ['mouseGridPosition']
+        self.keywords = ['mouseGridPosition', 'info']
 
         # POSITION LABEL
-        self.position_label = tk.Label(
-            self, text="( X:0 , Y:0 )")
-
-        # CENTER OF MASS SETTING
-        enableComDrawing = tk.IntVar(self)
-        enableComDrawing.set(0)
-        self.comCheckButton = tk.Checkbutton(self, text="Draw center of mass ",
-                                             variable=enableComDrawing,
-                                             onvalue=1, offvalue=0)
-
-        # CENTER OF GEOMETRY SETTING
-        enableCogDrawing = tk.IntVar(self)
-        enableCogDrawing.set(1)
-        self.cogCheckButton = tk.Checkbutton(self,
-                                             text="Draw geometrical center",
-                                             variable=enableCogDrawing,
-                                             onvalue=1, offvalue=0)
-
+        self.position_label = tk.Label(self, text="( X:0 , Y:0 )")
         self.position_label.pack(side=tk.RIGHT)
-        # self.comCheckButton.pack(side=tk.LEFT)
-        # self.cogCheckButton.pack(side=tk.LEFT)
-
+        # Info LABEL
+        self.info_label = tk.Label(self, text="")
+        self.info_label.pack(side=tk.LEFT)
+        
+        self._datacontroller.registerData('info', "")
         self._datacontroller.registerObserver(self, self.keywords)
+
 
     def dataUpdate(self, key, data):
         if key is 'mouseGridPosition':
@@ -47,3 +33,5 @@ class BottomBarWidget(tk.Frame):
                 (q, r) = data
                 self.position_label.config(
                     text=("( X:" + str(q) + " , " + "Y:" + str(r)) + ")")
+        if key is 'info':
+            self.info_label.config(text=str(data))
